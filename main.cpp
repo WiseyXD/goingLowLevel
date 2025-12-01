19056206b8dcba060f5911092a3c8ee2d014b64d
WiseyXD <aryan.s.nag@gmail.com>
2025-12-01 22:36:55 +0100

    build : learning C++

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+int main()
+{
+
+
+
+cout << *p << endl; // Output: 3 (value of the third element of the array)}
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+int main(){
+    int n;
+    cout >> "Size of the array ? ";
+    cin >> n
+
+}
+
