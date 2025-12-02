11772f6cf77f74162ae1b59d96ae1f5cbd785d1c
WiseyXD <aryan.s.nag@gmail.com>
2025-12-02 22:01:52 +0100

    build : learning oopmInCPP

--- test.cpp
+++ test.cpp
@@ -3,10 +3,14 @@
 #include <sstream>
 
 using namespace std;
-int main(){
-    int n;
-    cout >> "Size of the array ? ";
-    cin >> n
+int main() {
+  int n;
+  cout << "Size of the array ? ";
+  int size = 5;
 
+  for (int i = 0; i < 5; i++) {
+    cout << " TEst";
+  }
+
+  return 0;
 }
-
