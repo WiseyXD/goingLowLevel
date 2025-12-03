271ce43ec0fa12601036ff67484ac020a4407fbd
WiseyXD <aryan.s.nag@gmail.com>
2025-12-03 15:04:43 +0100

    build : classes and objects in c++

---
+++ oopmInCPP/basics.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee {
+public:
+  int Age;
+  string Name;
+  string Address;
+  int WorkEx;
+
+  Employee(string name, int age, string address, int workEx) {
+    Age = age;
+    Name = name;
+    Address = address;
+  }
+
+  void introduceYourself() {
+    cout << "Name : " << Name << endl;
+    cout << "Age : " << Age << endl;
+    cout << "Work Experience: " << WorkEx << endl;
+  }
+};
+
+int main() {
+  Employee emp1 = Employee("Aryan", 22, "Dresden", 1);
+  emp1.introduceYourself();
+  return 0;
+}
